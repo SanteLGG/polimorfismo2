791189a930df1d4bb0018f47bf6cd6de3507c9e9
Luis Guilherme Giannina Sante <57763042+SanteLGG@users.noreply.github.com>
2022-08-10 12:22:36 -0300

    Add files via upload

---
+++ aulapolimorfismo2/src/aulapolimorfismo2/Aulapolimorfismo2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aulapolimorfismo2;
+
+/**
+ *
+ * @author luisg
+ */
+public class Aulapolimorfismo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cachorro m = new Cachorro();
+        m.reagir("oi");
+        m.reagir("Vai apanhar");
+        m.reagir(11, 45);
+        m.reagir(21, 00);
+        m.reagir(true);
+        m.reagir(false);
+        m.reagir(2, 12.5f);
+        m.reagir(17, 4.5f);
+        
+        
+    }
+    
+}
---
+++ aulapolimorfismo2/src/aulapolimorfismo2/Lobo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulapolimorfismo2;
+
+/**
+ *
+ * @author luisg
+ */
+public class Lobo extends Mamifero{
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de lobo");
+    }
+    
+    
+}
